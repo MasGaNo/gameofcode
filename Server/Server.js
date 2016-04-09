1dfd2fe7b0c2ee43f2ae08c8802d4cb356b489fd
Joachim Justh <joachim.justh@rea-group.com>
2016-04-09 02:29:30 +0200

    Add Lodash and bodyParser

--- Server/Server.js
+++ Server/Server.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function isNullOrUndefined(value) {
+	return _.isNull(value) === true || _.isUndefined(value) === true
+}
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
 app.post('/trafficPlanner', function(req, res) {
-	res.send(req.body);
+	var uid = req.body.uid,
+			position = req.body.positionFrom;
+
+
 });
 
 app.listen(3000, function () {
