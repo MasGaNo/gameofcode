277f93fcccefdf5c4747eef35016633fbbb23697
Joachim Justh <joachim.justh@rea-group.com>
2016-04-09 02:42:12 +0200

    Move into TypeScript

--- Server/Server.js
+++ Server/Server.js
@@ -1,24 +1,27 @@
+"use strict";
+/// <reference path="Scripts/typings/express/express.d.ts"/>
 var express = require('express');
+/// <reference path="Scripts/typings/body-parser/body-parser.d.ts"/>
 var bodyParser = require('body-parser');
+/// <reference path="Scripts/typings/lodash/lodash.d.ts"/>
+var _ = require('lodash');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 function isNullOrUndefined(value) {
-	return _.isNull(value) === true || _.isUndefined(value) === true
+    return _.isNull(value) === true || _.isUndefined(value) === true;
 }
-
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+    res.send('Hello World!');
 });
-
-app.post('/trafficPlanner', function(req, res) {
-	var uid = req.body.uid,
-			position = req.body.positionFrom;
-
-
+app.post('/trafficPlanner', function (req, res) {
+    var uid = req.body.uid, position = req.body.position;
+    if (isNullOrUndefined(uid) || isNullOrUndefined(position)) {
+        res.status();
+        res.send();
+    }
 });
-
 app.listen(3000, function () {
-	console.log('The Mobiliteit Server is running.');
+    console.log('The Mobiliteit Server is running.');
 });
+//# sourceMappingURL=server.js.map
--- Server/server.ts
+++ Server/server.ts
@@ -1,6 +1,32 @@
-﻿import http = require('http');
-var port = process.env.port || 1337
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿/// <reference path="Scripts/typings/express/express.d.ts"/>
+import express = require('express');
+/// <reference path="Scripts/typings/body-parser/body-parser.d.ts"/>
+import bodyParser = require('body-parser');
+/// <reference path="Scripts/typings/lodash/lodash.d.ts"/>
+import _ = require('lodash')
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function isNullOrUndefined(value) {
+    return _.isNull(value) === true || _.isUndefined(value) === true
+}
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/trafficPlanner', function(req, res) {
+    var uid = req.body.uid,
+        position = req.body.position;
+    if (isNullOrUndefined(uid) || isNullOrUndefined(position)) {
+        res.status();
+        res.send();
+    }
+
+});
+
+app.listen(3000, function () {
+    console.log('The Mobiliteit Server is running.');
+});
